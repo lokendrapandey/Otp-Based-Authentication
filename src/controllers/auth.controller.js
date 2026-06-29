3e8300894d0510aabf01cddbea4882eca5dc6c28
lokendra pandey <lokendrapandey5278@gmail.com>
2026-06-29 06:32:03 +0530

    get all users

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -271,3 +271,22 @@ export async function verifyEmail(req,res){
         }
     })
 }
+
+
+
+export async function getAllUsers(req, res) {
+    try {
+        const users = await userModel.find().select("-password");
+
+        res.status(200).json({
+            success: true,
+            count: users.length,
+            users
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -23,4 +23,8 @@ authRouter.get("/logout-all", authController.logoutAll);
 
 //otp verification 
 authRouter.get("/verify-email", authController.verifyEmail);
+
+
+// get all users
+authRouter.get("/users", authController.getAllUsers);
 export default authRouter;
